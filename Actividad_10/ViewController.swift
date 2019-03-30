a97800620ec48663bdd5dad2ab2e58ffeab39326
Adrian Sandoval <al02813163@tecmilenio.mx>
2019-03-29 22:35:15 -0600

    Initial Commit

---
+++ Actividad_10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Actividad_10
+//
+//  Created by Adrian Alejandro Sandoval Perez on 3/29/19.
+//  Copyright © 2019 Adrian Alejandro Sandoval Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
